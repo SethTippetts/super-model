bfa6690c9487edfc04560aca8face20710495082
Seth Tippetts <sethtippetts@gmail.com>
2015-09-24 01:51:39 -0600

    Removing interfaces for plugin system

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import Promise from 'bluebird';
 import path from 'path';
 import loadModels from './models';
-import assignMethods from './methods';
 import assert from 'assert';
 import debug from 'debug';
-import interfaces from './interfaces';
 
 let log = debug('super-model');
 let initialized;
-let configured = false;
+let configured;
 let initPromise = Promise.fromNode(cb => (initialized = cb));
 
 export let getModels = (name) => initPromise
@@ -23,30 +21,22 @@ export default getModels;
 
 export function init(config, next) {
 
-  if (!configured) {
-    configured = true;
+  assert(!configured, 'Already configured');
+  configured = true;
 
-    if (!config.factory) {
-      config.factory = interfaces(config.plugin);
+  config = Object.assign({
+    dir: path.join(process.cwd(), 'models'),
+    factory: function(supermodel, cb) {
+      throw new Error('No interface set!');
+    },
+
+    validate: function(attr, value, cb) {
+
     }
+  }, config);
 
-    config = Object.assign({
-      dir: path.join(process.cwd(), 'models'),
-      factory: function(supermodel, cb) {
-        throw new Error('No interface set!');
-      },
-
-      validate: function(attr, value, cb) {
-
-      }
-    }, config);
-
-    // assignMethods(this, config);
-  }
-
-  return loadModels(config)
-    .nodeify(next);
-}
+  return loadModels(config).nodeify(initialized);
+};
 
 // Validation
 // Query/Body Coercion
